d2e523786e404c385239b72e88656973883dad42
PedroFurtado7 <88113287+PedroFurtado7@users.noreply.github.com>
2023-06-20 01:32:18 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!';
+let novaSenha = '';
+
+sizePassword.innerHTML = sliderElement.value;
+
+sliderElement.oninput = function() {
+  sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+  let pass = '';
+  for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
+
+  console.log(pass)
+  containerPassword.classList.remove("hide");
+  password.innerHTML = pass;
+  novaSenha = pass;
+}
+
+function copyPassword(){
+  alert("Senha copiada com sucesso!")
+  navigator.clipboard.writeText(novaSenha);
+}
+
+function togglePasswordVisibility() {
+  var passwordElement = document.getElementById("password");
+  var toggleButton = document.getElementById("toggle-button");
+
+  if (passwordElement.type === "password") {
+    passwordElement.type = "text";
+    toggleButton.textContent = "Ocultar senha";
+  } else {
+    passwordElement.type = "password";
+    toggleButton.textContent = "Mostrar senha";
+  }
+}
